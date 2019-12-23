f930b999dbe6486a8cd48942aff9adf5430a785d
shingchi <shingchi@qq.com>
2019-12-23 10:59:07 +0800

    Site updated: 2019-12-23 10:59:06

--- js/hung.js
+++ js/hung.js
@@ -9,18 +9,6 @@
 }(typeof self !== 'undefined' ? self : this, function () {
   var Hung = {
 
-    // 阅读进度
-    rp: function (selector) {
-      var progress = document.querySelector(selector);
-      var progressBar = progress.querySelector('.rProgress-bar');
-
-      progressBar.style.width = Hung._gw() + 'px';
-
-      window.addEventListener('scroll', ()=> {
-        progressBar.style.width = Hung._gw() + 'px';
-      });
-    },
-
     // 返回顶部
     gt: function (selector, offset) {
       var currentPosition, timer;
@@ -49,36 +37,24 @@
       });
     },
 
-    // 社会主义核心价值观
-    gsv: function () {
-      const words = ['富强', '民主', '文明', '和谐', '自由', '平等', '公正', '法治', '爱国', '敬业', '诚信', '友善'];
-      let i = 0;
-      var top, left, timer;
+    // 弹出菜单
+    tg: function (selector, nav) {
+      var trigger = document.querySelector(selector);
+      var nav = document.querySelector(nav);
 
-      document.addEventListener('click', (event) => {
-        top = event.pageY - 20;
-        left = event.pageX;
-        i = (i + 1) % words.length;
-        var node = document.createElement('span');
+      trigger.addEventListener('click', () => {
+        nav.classList.toggle('is-show');
+      });
+    },
 
-        node.textContent = words[i];
-        node.setAttribute('style', 'opacity:1;position:absolute;top:' + top + 'px;left:' + left + 'px;font-weight:bold;color:' + Hung._gc());
+    // 返回
+    gb: function (selector) {
+      var trigger = document.querySelector(selector);
 
-        document.body.appendChild(node);
+      if (!trigger) return;
 
-        // 淡出
-        var offset = 0;
-        var opacity = 1 / 15;
-
-        timer = setInterval(() => {
-          if (offset < 150) {
-            offset += 10;
-            node.style.top = (node.offsetTop - 10) + 'px';
-            node.style.opacity = node.style.opacity - opacity;
-          } else {
-            node.remove();
-          }
-        }, 100);
+      trigger.addEventListener('click', () => {
+        window.history.back();
       });
     },
 
@@ -94,22 +70,6 @@
         document.body.classList.toggle('is-dark');
         window.localStorage && window.localStorage.setItem('theme', document.body.classList.contains('is-dark') ? 'dark' : 'light');
       });
-    },
-
-    // 获取阅读进度比
-    _gw: function () {
-      var windowWidth = window.innerWidth;
-      var windowHeight = window.innerHeight;
-      var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-      var pageHeight = document.body.clientHeight;
-      var width = (scrollTop / (pageHeight - windowHeight) * windowWidth).toFixed();
-
-      return width;
-    },
-
-    // 随机 RGB
-    _gc: function () {
-      return 'rgb(' + Math.floor(Math.random() * 255) + ',' + Math.floor(Math.random() * 255) + ',' + Math.floor(Math.random() * 255) + ')';
     }
 
   };
